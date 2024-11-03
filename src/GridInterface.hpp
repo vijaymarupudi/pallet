fbb67f2a3bed693a252f2bc59693f5b91d47ec94
Vijay Marupudi <vijay@vijaymarupudi.com>
2024-11-02 21:32:35 -0400

    Add the raw interface code for the future just in case

--- src/GridInterface.hpp
+++ src/GridInterface.hpp
@@ -212,6 +212,74 @@ public:
   }
 };
 
+  // Raw bytes
+
+  // #include <termios.h>
+
+  // int fd = open("/dev/ttyACM0", O_NONBLOCK | O_RDWR);
+
+  // if (fd < 0) {
+  //   perror("Error: ");
+  //   exit(1);
+  // }
+
+  // struct termios params;
+  // tcgetattr(fd, &params);
+  // cfsetospeed(&params, 115200);
+  // cfsetispeed(&params, 115200);
+  // /* parity (8N1) */
+  // params.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
+  // params.c_cflag |=  (CS8 | CLOCAL | CREAD);
+
+  // /* no line processing */
+  // params.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG | IEXTEN);
+
+  // /* raw input */
+  // params.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK |
+  //                 INPCK | ISTRIP | IXON);
+
+  // /* raw output */
+  // params.c_oflag &= ~(OCRNL | ONLCR | ONLRET | ONOCR |
+  //                 OFILL | OPOST);
+
+  // params.c_cc[VMIN]  = 1;
+  // params.c_cc[VTIME] = 0;
+
+  // tcsetattr(fd, TCSANOW, &params);
+
+  // platform.watchFdIn(fd, [](int fd, void* ud) {
+  //   char buf[1024];
+  //   ssize_t len = read(fd, buf, 1024);
+
+  //   if (len < 0) {
+  //     perror("Error: ");
+  //     exit(1);
+  //   }
+
+  //   if (len % 3 != 0) {
+  //     fprintf(stderr, "Error in reading grid serial\n");
+  //   }
+
+  //   for (int i = 0; i < len; i += 3) {
+  //     unsigned char b1 = buf[i];
+  //     unsigned char b2 = buf[i + 1];
+  //     unsigned char b3 = buf[i + 2];
+
+  //     char buf[1024];
+
+
+  //     if (b1 == (2 << 4) + 0x00) {
+  //       snprintf(buf, 1024, "v%dl0Z", voiceNumber);
+  //       amy_play_message(buf);
+  //       // voiceNumber = (voiceNumber + 1) % nVoices;
+  //     }
+
+  //     if (b1 == (2 << 4) + 0x01) {
+  //       snprintf(buf, 1024, "v%dl1n%dZ", voiceNumber, scale(b2 + b3 * 16) % 127);
+  //       amy_play_message(buf);
+  //     }
+  //   }
+  // }, nullptr);
 
 
 
