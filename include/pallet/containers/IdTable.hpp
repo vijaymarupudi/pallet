50df75a1ff912ea82702ebd0e84713328ee1726a
Vijay Marupudi <vijay@vijaymarupudi.com>
2025-04-05 16:50:38 -0400

    Added IdTable iterators

--- include/pallet/containers/IdTable.hpp
+++ include/pallet/containers/IdTable.hpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <cstdint>
+#include <cstddef>
 #include <vector>
 #include <concepts>
 #include "containerUtils.hpp"
@@ -47,13 +48,20 @@ void recapContainer(Container& existing, size_t newCapacity) {
 }
 }
 
-  // TODO: items that have move constructors won't work properly because it won't be called when the Container is resizing
 
-template <class EntryType, template<class> class Container = std::vector, class Id = uint32_t>
-requires detail::IdTableContainerRequirements<Container<Id>, Id> &&
+template <class IdTableType>
+class IdTableIterator;
+
+template <class EntryType,
+          template<class> class Container = std::vector,
+          class IdType = uint32_t>
+requires detail::IdTableContainerRequirements<Container<IdType>, IdType> &&
   detail::IdTableContainerRequirements<Container<EntryType>, EntryType> &&
-  std::unsigned_integral<Id>
+  std::unsigned_integral<IdType>
 class IdTable {
+  friend class IdTableIterator<IdTable>;
+  using value_type = EntryType;
+  using Id = IdType;
 public:
 
   IdTable() {}
@@ -64,6 +72,14 @@ public:
       detail::recapContainer<EntryType>(storage, n);
       freeVector.reserve(n);
     }
+  }
+
+  size_t size() {
+    return storage.size() - freeVector.size();
+  }
+
+  size_t size() const {
+    return storage.size() - freeVector.size();
   }
 
   template <class T>
@@ -100,19 +116,19 @@ public:
 
     std::sort(other.freeVector.begin(), other.freeVector.end(), std::greater<Id>{});
 
-    for (size_t idTableIndex = 0;
-         idTableIndex < other.storage.size();
-         idTableIndex++) {
+    for (size_t idTableStorageIndex = 0;
+         idTableStorageIndex < other.storage.size();
+         idTableStorageIndex++) {
 
       auto& space = this->storage.emplace_back();
 
-      if (other.freeVector.size() != 0 && other.freeVector.back() == idTableIndex) {
+      if (other.freeVector.size() != 0 && other.freeVector.back() == idTableStorageIndex) {
         // empty
-        this->freeVector.push_back(idTableIndex);
+        this->freeVector.push_back(idTableStorageIndex);
         other.freeVector.pop_back();
       } else {
         // not empty
-        EntryType& entry = other.storage[idTableIndex].ref();
+        EntryType& entry = other.storage[idTableStorageIndex].ref();
         space.construct(std::move(entry));
       }
     }
@@ -129,16 +145,27 @@ public:
 
   ~IdTable() {
     std::sort(freeVector.begin(), freeVector.end(), std::greater<Id>{});
-    for (size_t idTableIndex = 0;
-         idTableIndex < this->storage.size(); idTableIndex++) {
-      if (freeVector.size() != 0 && freeVector.back() == idTableIndex) {
+    for (size_t idTableStorageIndex = 0;
+         idTableStorageIndex < this->storage.size(); idTableStorageIndex++) {
+      if (freeVector.size() != 0 && freeVector.back() == idTableStorageIndex) {
         freeVector.pop_back();
       } else {
         // not empty
-        this->storage[idTableIndex].destroy();
+        this->storage[idTableStorageIndex].destroy();
       }
     }
   }
+
+  IdTableIterator<IdTable> begin () {
+    std::sort(freeVector.begin(), freeVector.end(), std::greater<Id>{});
+    return IdTableIterator<IdTable> {this, 0, static_cast<std::ptrdiff_t>(freeVector.size() - 1)};
+  }
+
+  IdTableIterator<IdTable> end () {
+    return IdTableIterator<IdTable> {this, this->storage.size(),
+                                     static_cast<std::ptrdiff_t>(freeVector.size() - 1)};
+  }
+
 
 private:
   Container<Space<EntryType>> storage;
@@ -159,4 +186,36 @@ private:
   }
 
 };
+
+template <class IdTableType>
+struct IdTableIterator {
+
+  IdTableType* table;
+  std::size_t idTableStorageIndex;
+  std::ptrdiff_t freeVectorIndex;
+  
+  IdTableType::value_type& operator*() {
+    return (*table)[idTableStorageIndex];
+  }
+
+  bool operator==(const IdTableIterator& other) const {
+    return idTableStorageIndex == other.idTableStorageIndex;
+  }
+
+  IdTableIterator& operator++() {
+    while (true) {
+      idTableStorageIndex++;
+      if (idTableStorageIndex >= table->storage.size()) {
+        break;
+      }
+      else if (freeVectorIndex >= 0 && idTableStorageIndex == table->freeVector[freeVectorIndex]) {
+        freeVectorIndex--;
+      } else {
+        break;
+      }
+    }
+    return *this;
+  }
+};
+
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,21 @@
 #include <cstdio>
-#include "pallet/error.hpp"
-#include <memory>
-#include "pallet/memory.hpp"
+#include "pallet/containers/IdTable.hpp"
 
 
 int main()
 
 {
-  pallet::UniqueResource<int, decltype([](int fd){printf("here!\n");(void)fd;})> resource {3};
-  return 0;
+  pallet::containers::IdTable<int> table;
+
+  table.push(1);
+  auto id = table.push(2);
+  table.push(3);
+  auto id2 = table.push(4);
+  table.push(5);
+  table.free(id);
+  table.free(id2);
+
+  for (const auto& v : table) {
+    printf("%d\n", v);
+  }
 }
